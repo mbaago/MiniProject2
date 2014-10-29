b8ae1ea6d01fa03a84ab63e43e4b6f63e92a593e
Emil Jensen <estj92@gmail.com>
2014-10-29 12:36:03 +0100

    Added tokenizer class and implementation. Regex probably not correct.

---
+++ miniproject2/Tokenizer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace miniproject2
+{
+    /// <summary>
+    /// Taken from
+    /// http://sentiment.christopherpotts.net/code-data/happyfuntokenizing.py
+    /// </summary>
+    public static class Tokenizer
+    {
+        #region Regex Strings
+        private static string EmoticonString =
+            @"(?:" +
+            @"[<>]?" +
+            @"[:;=8]" +
+            @"[-o*']?" +
+            @"[\)\]\(\[dDpP/:\}\{@\|\\]" +
+            @"|" +
+            @"[\)\]\(\[dDpP/:\}\{@\|\\]" +
+            @"[-o*']?" +
+            @"[:;=8]" +
+            @"[<>]?" +
+            @")";
+
+        private static string PhoneNumbers =
+            @"(?:" +
+                @"(?:" +
+                    @"\+?[01]" +
+                    @"[\-\s.]*" +
+                @")?" +
+                @"(?:" +
+                    @"[\(]?" +
+                    @"\d{3}" +
+                    @"[\-\s.\)]*" +
+                @")?" +
+                @"\d{3}" +
+                @"[\-\s.]*" +
+                @"\d{4}" +
+            @")";
+
+        private static string HTMLTags = @"<[^>]>";
+
+        private static string TwitterUserName = @"(?:@[\w_]+)";
+
+        private static string TwitterHashTags = @"(?:#+[\w_]+[\w'_\-]*[\w_]+)";
+
+        private static string RemainingWordTypes =
+            @"(?:[a-z][a-z'\-_]+[a-z])" +
+            @"|(?:[+\-]?\d+[,/.:-]\d+[+\-]?)" +
+            @"|(?:[\w_]+)" +
+            @"|(?:\.(?:\s*\.){1,})" +
+            @"|(?:\S)";
+        #endregion
+
+        private static Regex EmoRegex { get; set; }
+        private static Regex RegexTokenizer { get; set; }
+
+        static Tokenizer()
+        {
+            EmoRegex = new Regex(EmoticonString, RegexOptions.Compiled);
+            string[] RegexStrings ={
+                                          PhoneNumbers,
+                                          EmoticonString,
+                                          HTMLTags,
+                                          TwitterUserName,
+                                          TwitterHashTags,
+                                          RemainingWordTypes
+                                      };
+
+            string regString = string.Join("|", RegexStrings);
+            RegexTokenizer = new Regex(regString, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        }
+
+        public static IEnumerable<string> Tokenize(string s)
+        {
+            var matchList = RegexTokenizer.Matches(s);
+            var matches = matchList.Cast<Match>()
+                .Select(m => m.Value);
+            var lowered = matches
+                .Select(m => EmoRegex.IsMatch(m) ? m : m.ToLower());
+
+            return lowered;
+        }
+    }
+}

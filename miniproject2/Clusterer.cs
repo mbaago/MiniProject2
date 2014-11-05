f17e0ead8001fe45291ef17d8cd9889a76397dc7
Emil Jensen <estj92@gmail.com>
2014-11-05 11:17:36 +0100

    Added clusterer class, added jaccard method.

---
+++ miniproject2/Clusterer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniproject2
+{
+    public class Clusterer
+    {
+        private double Jaccard(IEnumerable<Person> p1, IEnumerable<Person> p2)
+        {
+            var cap = p1.Intersect(p2);
+            var cup = p1.Union(p2);
+            var capSize = cap.Count();
+            var cupSize = cup.Count();
+
+            return capSize / cupSize;
+        }
+
+
+    }
+}
